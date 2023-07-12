3623f774a08ebfeb0ba1e94d4040d8bfbf78aa2b
SAW <119912370+Saw-Eh-Kalane-Phaung@users.noreply.github.com>
2023-07-12 09:34:10 +0700

    Add files via upload

---
+++ SE331-Lab 1.1/components/ProductDisplay.js
@@ -0,0 +1,185 @@
+app.component("product-display", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  template: `
+      <div class="product-display">
+        <div class="product-container">
+          <div class="product-image">
+            <img :src="image">
+          </div>
+          <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inStock">In stock</p>
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+            <product-component :details="details"></product-component>
+            <div
+              v-for="(variant, index) in variants"
+              :key="variant.id"
+              @mouseover="updateVariant(index)"
+              class="color-circle"
+              :style="{ backgroundColor: variant.color }">
+            </div>
+            <button
+              class="button"
+              :class="{ disabledButton: !inStock }"
+              :disabled="!inStock"
+              v-on:click="addToCart">
+              Add to Cart
+            </button>
+            <button
+              class="button"
+              v-on:click="clearCart">
+              Reduce from Cart
+            </button>
+            <button
+              class="button"
+              v-on:click="clearAllCart">
+              Reduce All From Cart
+            </button>
+          </div>
+          <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+          <review-form @review-submitted="addReview"></review-form>
+        </div>
+      </div>
+    `,
+  data() {
+    return {
+      product: "Shoes",
+      brand: "SE 331",
+      inventory: 100,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+      activeClass: true,
+      selectedVariant: 0,
+      reviews: [],
+      onSale: true,
+    };
+  },
+  methods: {
+    addToCart() {
+      this.$emit("add-to-cart", this.variants[this.selectedVariant].id);
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+    clearCart() {
+      this.$emit("clear-cart", this.variants[this.selectedVariant].id);
+    },
+    clearAllCart() {
+      this.$emit("clear-all-cart");
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+    addReview(review) {
+      this.reviews.push(review);
+    },
+  },
+  computed: {
+    title() {
+      if (!this.onSale) {
+        return this.brand + " " + this.product;
+      }
+      return this.brand + " " + this.product + " is on sale.";
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity > 0;
+    },
+    // shipping premium or general customer
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      }
+      return 30;
+    },
+  },
+});
+
+app.component("product-component", {
+  props: {
+    details: {
+      type: Array,
+      required: true,
+    },
+  },
+  template: `
+      <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+      </ul>
+    `,
+});
+
+app.component("review-form", {
+  template: `
+      <form class="review-form" @submit.prevent="onSubmit">
+        <h3>Leave a review</h3>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name">
+        <label for="review">Review:</label>
+        <textarea id="review" v-model="review"></textarea>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+        <label for="rec">Would you recommend this product?:</label>
+        <select id="rec" v-model="rec">
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>
+        </select>
+        
+        <input class="button" type="submit" value="Submit">
+      </form>
+    `,
+  data() {
+    return {
+      name: "",
+      review: "",
+      rating: null,
+      rec: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.name === "" || this.review === "" || this.rating === null) {
+        alert("Review is incomplete. Please fill out every field.");
+        return;
+      }
+      let productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating,
+        rec: this.rec,
+      };
+      this.$emit("review-submitted", productReview);
+      this.name = "";
+      this.review = "";
+      this.rating = null;
+      this.rec = "";
+    },
+  },
+});
---
+++ SE331-Lab 1.1/main.js
@@ -0,0 +1,63 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Shoes",
+      brand: "SE 331",
+      inventory: 100,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+      selectedVariant: 0,
+      cart: ["2234: 0"],
+      cart2: ["2235: 0"],
+      onSale: true,
+      premium: true,
+      countF: 0,
+      countS: 0,
+    };
+  },
+  methods: {
+    updateCart(id) {
+      if (id == 2234) {
+        this.countF += 1;
+        this.cart.splice(0);
+        this.cart.push(id + ": " + this.countF);
+      } else if (id == 2235) {
+        this.countS += 1;
+        this.cart2.splice(0);
+        this.cart2.push(id + ": " + this.countS);
+      }
+    },
+    clearCart(id) {
+      if (id == 2234 && this.countF > 0) {
+        this.countF -= 1;
+        this.cart.splice(0);
+        this.cart.push(id + ": " + this.countF);
+      } else if (id == 2235 && this.countS > 0) {
+        this.countS -= 1;
+        this.cart2.splice(0);
+        this.cart2.push(id + ": " + this.countS);
+      }
+    },
+    clearAllCart() {
+      this.cart.splice(0);
+      this.cart2.splice(0);
+      this.countF = 0;
+      this.countS = 0;
+      this.cart.push("2234: " + this.countF);
+      this.cart2.push("2235: " + this.countS);
+    },
+  },
+});
